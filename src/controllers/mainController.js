6cd00608ec28a59ee552af55b29b85f0131319f6
JaiderValencia <janvame05@gmail.com>
2023-02-25 23:34:35 -0500

    Agrego la data

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,24 @@
+// natives modules
+const fs = require("fs");
+const path = require("path");
+
+
+// data path
+const peoplePath = path.join(__dirname, "../data/people.json");
+
+
+// data import and convert
+const people = fs.readFileSync(peoplePath);
+const peopleConvert = JSON.parse(people);
+
+
+// controller
 const controller = {
     index: (req, res) => {
-        res.render("index");
+        res.render("index", { people: peopleConvert });
     }
 };
+
 
 // export controller
 module.exports = controller;
